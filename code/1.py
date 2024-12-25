03358ca79385dd75d45c248927141122b41e0a13
Arina Turchik <arina.turchik@mail.ru>
2024-12-25 16:34:34 +0500

    firs commit

---
+++ code/1.py
@@ -0,0 +1,20 @@
+def hero_hp():
+    hp = 100
+
+    def upd_hp(n):
+        nonlocal hp
+        hp += n
+        if hp>100:
+            hp=100
+        if hp<0:
+            hp=0
+        return hp
+    return upd_hp
+
+hero=hero_hp()
+print('Введите, сколько раз будет изменяться HP героя(целое натуральное число):')
+r=int(input())
+for i in range(r+1):
+    print('Введите изменение HP героя(для получения урона используйте отрицательные числа, для лечения положительные):')
+    n=int(input())
+    print(hero(n))
